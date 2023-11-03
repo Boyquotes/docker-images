5097936e0ac4a6141b87e9e08a503c3c3e9ebcc4
Boyquotes <webmaster@linux-live-cd.org>
2023-11-03 12:22:07 +0100

    rotki works

--- rotki/entrypoint.sh
+++ rotki/entrypoint.sh
@@ -4,5 +4,16 @@ ls -l
 ls -l ../
 source /usr/local/bin/virtualenvwrapper.sh
 #mkvirtualenv rotki -p /usr/bin/python3.10 &&
+echo 'VIRTUAL ENV'
+echo $VIRTUAL_ENV
 workon rotki
+echo $VIRTUAL_ENV
+cd ..
+pwd
+pip3 install -e .
+ls
+cd -
+pwd
 pnpm run dev:web
+tail -f /dev/null
+# pnpm run dev:web
